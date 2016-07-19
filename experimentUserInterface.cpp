d76fb23619a8ad0fab9007f626b6d447acd33828
USC BBDL <git@valerolab.org>
2016-07-19 15:24:49 -0700

    working communication between python and c++

--- experimentUserInterface.cpp
+++ experimentUserInterface.cpp
@@ -1,13 +1,85 @@
 // experimentUserInterface.cpp : Defines the entry point for the console application.
 //
 #include <stdio.h>
-#include <windows.h>
 #include <iostream>
+#include <fstream>
 #include <conio.h>
 #include <utilities.h>
+#include "zmq.hpp"
+#include <string>
+#include <pthread.h>
+#include "ipc.h"
+#include <map>
+#ifndef _WIN32
+#else
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <Windows.h>
+
+#define sleep(n) Sleep(n)
+#endif
+
+#define NUM_THREADS 2
+IPC* test = new IPC();
+zmq::context_t context(1);
+zmq::socket_t socket_1(context, ZMQ_REP);
+std::map<int, std::vector<int> > record;
+
+using namespace std;
+
+void *test_1(void*) {
+    cout<<"test1 starts\n"<<endl;
+    while(true) {
+        if(test->wasDataReceived()) {
+            Sleep(1000);
+            //if IPC gets data from python, process it
+            //have to in IPC 
+            //int element = stoi(test.testingData)+10;
+            //record.insert( std::pair<int, std::vector<int> > (test->getVector_data()[6], test->getVector_element()) );
+            std::string textinput = test->generateString(test->getVector_data());
+            ofstream myfile ("example.txt", ios_base::app);
+            //myfile.open("example.txt", ofstream::app);
+            if(myfile.is_open()) {
+                std::cout<<textinput<<std::endl;
+                myfile << textinput << endl;
+                myfile.close();
+            }
+            //writing text file is done so clear all the data from the vectors and send OK message to IPC
+            //so that Python know when file is updated
+            test->clearVector_element(); // clear the vector so HR does not get duplicate data.
+
+            test->sendData(10);
+        }
+    }
+    pthread_exit(NULL);
+}
+
+void *runServer(void*) {
+
+    cout<<"run server starts\n"<<endl;
+    test->startServer();
+
+   pthread_exit(NULL);
+   return NULL;
+}
+
+
+
+
 
 int main()
 {
+
+      int i, j;
+      pthread_t thread_1, thread_2;
+      pthread_create(&thread_1, NULL, test_1, NULL);
+      pthread_create(&thread_2, NULL, runServer, NULL);
+      pthread_join(thread_1, (void **)&i);
+      pthread_join(thread_2, (void **)&j);
+
+
+
+    /*
     printf("Press Spc to move forward\n\n");
     printf("Press Esc to terminate\n");
     printf("Initialization; Next stage is Motors Winding up\n");
@@ -31,6 +103,8 @@ int main()
            }
         }
     }
+    */
+   delete test;
    return 0;
 }
 
---
+++ zmqPython.py
@@ -0,0 +1,50 @@
+import zmq
+
+class python_server(object):
+	context = zmq.Context()
+	socket = context.socket(zmq.REQ)
+	def __init__(self, socket):
+		self.socket = socket
+		self.socket.connect("tcp://localhost:5555")
+
+	def sendMessage(self, message):
+		socket.send(message)
+	
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+
+import json
+row = [1,2,3,4,5,6]
+row_json = json.dumps(row)
+print row_json
+incrementor = 0
+IDNumber = 0
+
+for it in range(1000):
+	user_input = ""
+	if user_input == "":
+
+		hello = python_server(socket)
+		row.append(IDNumber)
+		numJson = json.dumps(row)
+		hello.sendMessage(numJson)
+		print "waht we are sending"
+		print row
+		print "-------"
+		del row [6]
+		IDNumber += 1
+
+		message = socket.recv()
+		print message
+		##dataVector = []
+
+		##dataVector = message.split(',')
+		##dataVector_1 = []
+		##for it in dataVector:
+		##	integer = int(it)
+		##	dataVector_1.append(integer)
+
+		##print dataVector_1
+	else:
+		message = socket.recv()
+		print message
