6b4fdd5eb2fdfd91342394ff00baedcc71cb194b
USC BBDL <git@valerolab.org>
2016-07-25 13:04:05 -0700

    IPC cpp and h

--- ipc.h
+++ ipc.h
@@ -1,21 +1,46 @@
-#pragma once
 #ifndef IPC_H_
 #define IPC_H_
-#include "ipc.h"
-#include <pthread.h>
+#include "IPC.h"
 
 
 //include these for socket programming
 #include "zmq.hpp"
-#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
 #ifndef _WIN32
 #include <unistd.h>
 #else
+#include <windows.h>
+
 #define sleep(n)  Sleep(n)
 #endif
+// This is everything that needs to be included if I use a socket
+// // Add the "Ws2_32.lib" to the LINKER settings
+// #pragma comment(lib, "Ws2_32.lib")
 
+// // STD console header files
+// #include <sdkddkver.h>
+// #include <conio.h>
+// #include <stdio.h>
+
+// // SOCKET header files
+// #include <WinSock2.h>
+// #include <Windows.h>
+// #include <iostream>
+
+// #define SCK_VERSION2 0x0202
 
 class IPC {
+private:
+	zmq::context_t context;
+	zmq::socket_t socket;
+	zmq::message_t request;
+	std::vector<int> vector_data; //this is vector data file that is sent to hand c++ so it can process it.
+	std::vector<int> csv_vector; //it can be used in order to generate csv file.
+	bool isReceieved = false;
+	int toPython;
+
 
 public:
 
@@ -39,21 +64,5 @@ public:
 
 	std::string testingData;
 
-    std::vector<int> getVector_element();
-
-    void clearVector_element();
-
-    std::vector<int> getVector_data();
-
-    private:
-	zmq::context_t context;
-	zmq::socket_t socket;
-	zmq::message_t request;
-	std::vector<int> vector_data; //this is vector data file that is sent to hand c++ so it can process it.
-	std::vector<int> csv_vector; //it can be used in order to generate csv file.
-	bool isReceieved;
-	int toPython;
-    std::vector<int> vector_element;
-    CONDITION_VARIABLE ConditionVar;
 };
 #endif
