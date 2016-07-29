41a17e3da9df04c70b8700b9894d48921562e7a2
USC BBDL <git@valerolab.org>
2016-07-29 16:29:46 -0700

    changed data type from integer from float64 and function accordingly

--- ipc.cpp
+++ ipc.cpp
@@ -5,6 +5,13 @@
 #include "ipc.h"
 #include <string>
 #include <fstream>
+
+#include <random>
+#include <windows.h>
+#include <conio.h>
+#include <motorControl.h>
+#include <math.h>
+
 #ifndef _WIN32
 #include <unistd.h>
 #else
@@ -16,61 +23,30 @@ void IPC::startServer(){
 
     socket.bind ("tcp://*:5555");
     while (true) {
-      /*zmq::message_t request;
-      // Wait for next request from client
-      socket.recv (&request);
-      //getting string type(json) from python and creating vector of integer
-      //so it can get processed with c++
-      //vector_data is private vector that can be accessed through hands code
-      //vector_data is private and accessed by hand code
-      isReceieved = true;
-      vector_data = generateVector(std::string(static_cast<char*>(request.data()), request.size()));
-      std::string outMessage = generateString(vector_data);
-      testingData = outMessage;
-      //Communication between IPC and hit and run starts from here
-      //getting csv file from hit and run
-      //getting the signal so it knows if csv file was updated.
-      //eventually it sends string to Hit and Run and get the result from it.
-      std::cout<<outMessage<<std::endl;
-      Sleep(2000);
-      //wait until getting the signal from hit and run with measured data
-      
-      //outMessage = std::to_string(toPython);
-      std::ostringstream convert;
-      convert << toPython;
-      outMessage = convert.str();
-      Sleep(2000);
-      // Send the string to python
-      zmq::message_t reply (outMessage.size());
-      const void * a = outMessage.c_str();
-      memcpy (reply.data(), a, outMessage.size());
-      socket.send(reply);
-      isReceieved = false; // reset it false  
-      */
-
 
       zmq::message_t request;
       socket.recv (&request);
       //IPC gets message from python and make it vector so that HR can process the data.
-      vector_data = generateVector(std::string(static_cast<char*>(request.data()), request.size()));
-      for(int i = 0; i < vector_data.size()-1; i++) {
-          vector_element.push_back(vector_data[i]);
-      }
+      generateVector(std::string(static_cast<char*>(request.data()), request.size()));
       isReceieved = true; //ipc gets data and makes it vector and HR gets it after that
       //this is where you have to wait until  so IPC can process and sending the data.
       
-      
-      
-      
-      
-      
-      std::string outMessage = "DONE";
-      zmq::message_t reply (outMessage.size());
-      const void * a = outMessage.c_str();
-      memcpy (reply.data(), a, outMessage.size());
-      Sleep(1000);
-      socket.send(reply);
-      isReceieved = false; // reset it false  
+
+      //sending flag is good here.
+      if(toPython == " ") {
+          while(toPython == " ") {
+          
+          }
+      }else {
+          zmq::message_t reply (toPython.size());
+          const void * a = toPython.c_str();
+          memcpy (reply.data(), a, toPython.size());
+          Sleep(1000);
+          socket.send(reply);
+          isReceieved = false; // reset it false  
+          toPython = " "; // initialize string to avoid duplicate data to python
+      }
+
 
     }
 }
@@ -94,7 +70,7 @@ bool IPC::wasDataReceived(){
   * Get the data from the Python server and store it as a vector.
   * Possibly return the vector.......???
   */
-std::vector<int> IPC::getData(){
+float64 * IPC::getData(){
   //when IPC get the message(data) from python,
   //IPC also process it and send it to HIT AND RUN
   return vector_data;
@@ -105,27 +81,41 @@ std::vector<int> IPC::getData(){
   * Gets the data from sendVector in HitRun and sends it to the 
   * Python server. 
   */
-int IPC::sendData(int integerData){
+std::string IPC::sendData(float64 data_to_python[MUSCLE_NUM]){
   //when HIT AND RUN send the data
-  toPython = integerData;
-  return toPython;
-}
-std::vector<int> IPC::generateVector(std::string python_data) {
+    //formulate string form to send data through socket
+    std::stringstream ss;
+    for(int i = 0; i < MUSCLE_NUM; i++) {
+       if(i != MUSCLE_NUM-1){
+           ss << data_to_python[i] << ",";
+       }else{
+           ss << data_to_python[i];
+       }
+    }
 
-    std::vector<int> data;
+    std::string outMessage;
+    ss >> outMessage;
+    toPython = outMessage;
+
+    return outMessage;
+
+}
+float64 * IPC::generateVector(std::string python_data) {
+    
+    int index = 0;
     for(std::string::iterator it = python_data.begin(); it != python_data.end(); ++it) {
         if(isdigit(*it)) {
-            int element = *it - '0';
-             data.push_back(element);
+            vector_data[index] = (float)(*it - '0');
+            index++;
         }
     }
-    return data;
+    return vector_data;
 }
-std::string IPC::generateString(std::vector<int> vector_c) {
+std::string IPC::generateString(float64 vector_c[MUSCLE_NUM]) {
     
     std::stringstream ss;
-     for(unsigned int i = 0; i < vector_c.size(); i++) {
-       if(i != vector_c.size()-1){
+     for(unsigned int i = 0; i < MUSCLE_NUM; i++) {
+       if(i != MUSCLE_NUM-1){
            ss << vector_c[i] << ",";
        }else{
            ss << vector_c[i];
@@ -137,27 +127,16 @@ std::string IPC::generateString(std::vector<int> vector_c) {
 
      return outMessage;
 }
-void IPC::generateCSV(std::string path, std::vector<int> new_data) {
-
-      std::ofstream csv_file;
-      csv_file.open (path);
-      for(unsigned int i = 0; i < new_data.size(); i++) {
-        csv_file << new_data[i] << ",";
-        if(i == (new_data.size()-1)) {
-          csv_file << "\n";
-        } 
-      }
-
-      csv_file.close();
-}
-std::vector<int> IPC::getVector_element() {
-    return vector_element;
+float64 * IPC::getVector_element() {
+    return vector_data;
 }
 void IPC::clearVector_element() {
-    vector_element.clear();
-    vector_data.clear();
+    for(int i = 0; i < MUSCLE_NUM; i++) {
+        vector_data[i] = 0.0;
+    }
+
 }
-std::vector<int> IPC::getVector_data() {
+float64 * IPC::getVector_data() {
     return vector_data;
 }
 void IPC::false_isReceived() {
--- ipc.h
+++ ipc.h
@@ -1,13 +1,13 @@
 #pragma once
 #ifndef IPC_H_
 #define IPC_H_
-#include "ipc.h"
+//#include "ipc.h"
 #include <pthread.h>
-
-
+#include <NIDAQmx.h>
 //include these for socket programming
 #include "zmq.hpp"
 #include <vector>
+#include "utilities.h"
 #ifndef _WIN32
 #include <unistd.h>
 #else
@@ -27,33 +27,31 @@ public:
 
     bool wasDataReceived();
 
-    std::vector<int> getData(); // should the return type of this be vector instead or does
+    float64 * getData(); // should the return type of this be vector instead or does
                     // the vectorization of the data happen in the HitRun class
-    int sendData(int integerData); // not sure about return type on this either
+    std::string sendData(float64 data_to_python[MUSCLE_NUM]);
 	
-	std::vector<int> generateVector(std::string python_data);
+	float64 * generateVector(std::string python_data);
 
-	std::string generateString(std::vector<int> vector_c);
-
-	void generateCSV(std::string path, std::vector<int> new_data);
+	std::string IPC::generateString(float64 vector_c[MUSCLE_NUM]);
 
 	std::string testingData;
 
-    std::vector<int> getVector_element();
+    float64 * getVector_element();
 
     void clearVector_element();
 
-    std::vector<int> getVector_data();
+    float64 * getVector_data();
     void false_isReceived();
 
     private:
 	zmq::context_t context;
 	zmq::socket_t socket;
 	zmq::message_t request;
-	std::vector<int> vector_data; //this is vector data file that is sent to hand c++ so it can process it.
+	float64 vector_data[MUSCLE_NUM]; //this is vector data file that is sent to hand c++ so it can process it.
 	std::vector<int> csv_vector; //it can be used in order to generate csv file.
 	bool isReceieved;
-	int toPython;
-    std::vector<int> vector_element;
+	std::string toPython;
+    float64 vector_element[MUSCLE_NUM];
 };
 #endif
